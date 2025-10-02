eef4adfb0dd3fce6cae58a407d9ee663a4a423a6
Davide Nguyen <d.nguyen@gamediveai.com>
2025-10-03 01:42:15 +0200

    cleanup

--- Contents/mods/PerfectionsItems/media/lua/client/01_PI_Client.lua
+++
@@ -1,6 +0,0 @@
--- Perfection's Items - Client Side
--- Simplified client-side script
-
-PI.Utils.debugPrint("Client module loading...")
-
-PI.Utils.debugPrint("Client module loaded successfully")
--- Contents/mods/PerfectionsItems/media/lua/server/01_PI_Client.lua
+++
@@ -1,6 +0,0 @@
--- Perfection's Items - Server Side
--- Simplified server-side script
-
-PI.Utils.debugPrint("Server module loading...")
-
-PI.Utils.debugPrint("Server module loaded successfully")
