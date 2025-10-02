9e76d4d02e1419701301389f05212f05fef2e192
Davide Nguyen <d.nguyen@gamediveai.com>
2025-10-02 22:26:50 +0200

    cleanup for now, see if this fixes items

--- Contents/mods/PerfectionsItems/media/lua/client/Client.lua
+++ Contents/mods/PerfectionsItems/media/lua/client/Client.lua
@@ -6,9 +6,4 @@ local Utils = PerfectionsItems.Utils
 
 Utils.debugPrint("Client module loading...")
 
--- Initialize on game start
-Events.OnGameStart.Add(function()
-    Utils.debugPrint("Client initialized")
-end)
-
 Utils.debugPrint("Client module loaded successfully")
---
+++ Contents/mods/PerfectionsItems/media/lua/server/Client.lua
@@ -0,0 +1,9 @@
+-- Perfection's Items - Server Side
+-- Simplified server-side script
+
+-- Access shared utilities from global namespace (set by shared/Utils.lua)
+local Utils = PerfectionsItems.Utils
+
+Utils.debugPrint("Server module loading...")
+
+Utils.debugPrint("Server module loaded successfully")
